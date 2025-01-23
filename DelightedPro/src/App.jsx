b97c0a5f12020b0d16a75ba242d4b0f9c6eaf8d3
vamsi34 <vamsim7989@gmail.com>
2025-01-23 22:35:14 +0530

    body updated

--- DelightedPro/src/App.jsx
+++ DelightedPro/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './NavBar'
+import Body from './Body'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
      <NavBar/>
-     
+     <Body/>
 
     </>
   )
--- DelightedPro/src/NavBar.jsx
+++ DelightedPro/src/NavBar.jsx
@@ -1,16 +1,16 @@
 import React from 'react-dom'
+import './App.css'
 import { Fragment } from 'react';
 function NavBar(){
 
-
     return(
         <Fragment>
-           <div className={NavBar}>
+           <div  className="NavBar">
               <div className="image">
                 <img src="" alt="log" />
               </div>
-              <div className="searchbar">
-                <input type="text"  />
+              <div className="Searchbar">
+                <input type="text"  /><button>search</button>
               </div>
            </div>
         </Fragment>
